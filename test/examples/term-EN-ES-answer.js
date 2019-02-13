4f4fd09cdbc0a5d16f418cc19c0b78de4e2d4dc3
Javier <javierdwd@gmail.com>
2019-02-13 11:42:12 -0300

    Update examples files. Add "nation" example.

--- test/examples/term-EN-ES-answer.js
+++ test/examples/term-EN-ES-answer.js
@@ -1,89 +1,45 @@
-module.exports = {
-  noResults: false,
-  queryTerm: "answer",
-  words: [
-    {
-      term: "answer",
-      type: "noun",
-      translations: [
-        {
-          term: "respuesta",
-          type: "noun, feminine",
-          examples: [
-            {
-              phrase: "My teacher surely knows the answer.",
-              translation: "Seguramente mi profesora sabe la respuesta."
-            },
-            {
-              phrase: "We have already chosen the right answer.",
-              translation: "Ya hemos elegido la respuesta correcta."
-            }
-          ]
-        },
-        {
-          term: "contestación",
-          type: "noun, feminine",
-          examples: [
-            {
-              phrase: "The interviewee gave an answer to the question.",
-              translation: "El entrevistado dio una contestación a la pregunta."
-            }
-          ]
-        },
-        {
-          term: "solución",
-          type: "noun, feminine",
-          examples: [
-            {
-              phrase: "There is an extra booklet with all the answers.",
-              translation: "Hay un libro adicional con todas las soluciones."
-            }
-          ]
-        }
-      ]
-    },
-    {
-      term: "answer",
-      type: "verb",
-      translations: [
-        {
-          term: "responder (a algo/algn.)",
-          type: "verb",
-          examples: [
-            {
-              phrase: "The receptionist will answer all the queries.",
-              translation: "El recepcionista responderá a todas las preguntas."
-            },
-            {
-              phrase: "The teacher answered the question raised in class.",
-              translation:
-                "El profesor respondió a la cuestión planteada en clase."
-            }
-          ]
-        },
-        {
-          term: "contestar",
-          type: "verb",
-          examples: [
-            {
-              phrase: "The student answered the questions correctly.",
-              translation:
-                "El estudiante contestó a las preguntas sin equivocarse."
-            }
-          ]
-        },
-        {
-          term: "atender algo",
-          type: "verb",
-          examples: [
-            {
-              phrase: "The information office answers all queries.",
-              translation:
-                "La oficina de información atiende todas las consultas."
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
+module.exports = { noResults: false,
+  queryTerm: 'answer',
+  words: 
+   [ { term: 'answer',
+       type: 'noun',
+       additionalInfo: { plural: 'answers' },
+       translations: 
+        [ { term: 'respuesta',
+            type: 'noun, feminine',
+            examples: 
+             [ { phrase: 'My teacher surely knows the answer.',
+                 translation: 'Seguramente mi profesora sabe la respuesta.' },
+               { phrase: 'We have already chosen the right answer.',
+                 translation: 'Ya hemos elegido la respuesta correcta.' } ] },
+          { term: 'contestación',
+            type: 'noun, feminine',
+            examples: 
+             [ { phrase: 'The interviewee gave an answer to the question.',
+                 translation: 'El entrevistado dio una contestación a la pregunta.' } ] },
+          { term: 'solución',
+            type: 'noun, feminine',
+            examples: 
+             [ { phrase: 'There is an extra booklet with all the answers.',
+                 translation: 'Hay un libro adicional con todas las soluciones.' } ] } ] },
+     { term: 'answer',
+       type: 'verb',
+       additionalInfo: { verbs: [ 'answered', 'answered' ], context: 'sb./sth.' },
+       translations: 
+        [ { term: 'responder (a algo/algn.)',
+            type: 'verb',
+            examples: 
+             [ { phrase: 'The receptionist will answer all the queries.',
+                 translation: 'El recepcionista responderá a todas las preguntas.' },
+               { phrase: 'The teacher answered the question raised in class.',
+                 translation: 'El profesor respondió a la cuestión planteada en clase.' } ] },
+          { term: 'contestar',
+            type: 'verb',
+            examples: 
+             [ { phrase: 'The student answered the questions correctly.',
+                 translation: 'El estudiante contestó a las preguntas sin equivocarse.' } ] },
+          { term: 'atender algo',
+            type: 'verb',
+            examples: 
+             [ { phrase: 'The information office answers all queries.',
+                 translation: 'La oficina de información atiende todas las consultas.' } ] } ] } ] }
