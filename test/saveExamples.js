8422427f37ea5e7cdf3bdc3491dde5ea06af2f64
Javier <javierdwd@gmail.com>
2019-02-13 11:39:21 -0300

    Add a group of functions to easily write examples files.

---
+++ test/saveExamples.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const iconv = require("iconv-lite");
+const axios = require("axios");
+const linguee = require("../lib/linguee");
+util = require("util");
+
+const term = "";
+const saveRAW = false;
+const saveJS = false;
+const langFrom = "english";
+const langTo = "spanish";
+
+function writeRAW() {
+  let url = `https://www.linguee.com/`;
+  url += `${langFrom}-${langTo}/search/search`;
+  url += `?source=auto&ajax=1&query=${term}`;
+
+  axios
+    .get(url, { responseType: "arraybuffer" })
+    .then(response => {
+      const data = iconv.decode(Buffer.from(response.data), "ISO-8859-15");
+
+      fs.writeFile(
+        `${__dirname}/examples/term-EN-ES-${term}.html`,
+        data,
+        { flag: "w" },
+        err => {
+          if (err) throw err;
+          console.log("The HTML file has been saved. Term: " + term);
+        }
+      );
+    })
+    .catch(error => {
+      reject(error);
+    });
+}
+
+async function writeJS() {
+  const data = await linguee.translate(term, "english", "spanish");
+
+  fs.writeFile(
+    `${__dirname}/examples/term-EN-ES-${term}.js`,
+    `module.exports = ${util.inspect(data, {
+      depth: null,
+      compact: false
+    })}`,
+    { flag: "w" },
+    err => {
+      if (err) throw err;
+      console.log("The JS file has been saved. Term: " + term);
+    }
+  );
+}
+
+if (term !== "") {
+  if (saveRAW) {
+    writeRAW();
+  }
+  if (saveJS) {
+    writeJS();
+  }
+}
