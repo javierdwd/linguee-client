bce4e5fe223400277e3abeebf60ba4af8352b942
Javier <javierdwd@gmail.com>
2019-04-08 10:25:10 -0300

    Update to make use of the new libraries

--- test/linguee-test.js
+++ test/linguee-test.js
@@ -2,19 +2,6 @@ const assert = require("assert");
 const linguee = require("../lib/linguee");
 
 describe("Linguee", () => {
-  describe("createUrl()", () => {
-    it("should prevents usage with an empty term", () => {
-      assert.throws(linguee.createUrl, /Empty/);
-    });
-
-    it("should return a fulfilled translation URL", () => {
-      assert.deepStrictEqual(
-        linguee.createUrl("hello", "english", "spanish"),
-        "https://www.linguee.com/english-spanish/search/search?source=auto&ajax=1&query=hello"
-      );
-    });
-  });
-
   describe.skip("translate()", function() {
     this.timeout(4000);
 
