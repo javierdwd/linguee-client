1071429f074d8c941a9bb51a039502cc578fca1c
Javier <javierdwd@gmail.com>
2020-03-08 01:33:06 -0300

    Set the decoding charset based on server response's  headers

--- test/linguee-test.js
+++ test/linguee-test.js
@@ -2,8 +2,8 @@ const assert = require("assert");
 const linguee = require("../lib/linguee");
 
 describe("Linguee", () => {
-  describe.skip("translate()", function() {
-    this.timeout(4000);
+  describe.skip("translate()", function () {
+    this.timeout(5000);
 
     it("should translate hello from english to spanish (hola)", async () => {
       const translation = await linguee.translate(
@@ -12,6 +12,24 @@ describe("Linguee", () => {
         "spanish"
       );
       assert.deepStrictEqual(translation.words[0].translations[0].term, "hola");
+    });
+
+    it("should translate song from english to spanish (canción)", async () => {
+      const translation = await linguee.translate(
+        "song",
+        "english",
+        "spanish"
+      );
+      assert.deepStrictEqual(translation.words[0].translations[0].term, "canción");
+    });
+
+    it("should translate hello from english to russian (привет)", async () => {
+      const translation = await linguee.translate(
+        "hello",
+        "english",
+        "russian"
+      );
+      assert.deepStrictEqual(translation.words[0].translations[0].term, "привет!");
     });
   });
 });
