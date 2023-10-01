d5ade3f37ebfc9d4e57b86d9aec70a7f504eaf39
Javier Van Breedan <javierdwd@gmail.com>
2023-10-01 19:58:00 -0300

    [Linguee] Remove direct access to Utils/Langs using the Linguee Object

--- lib/linguee.ts
+++ lib/linguee.ts
@@ -6,55 +6,50 @@ import Endpoint from './utils/Endpoint';
 import ExtractorsFactory from './extractors/ExtractorsFactory';
 import LingueeExtractor from './extractors/LingueeExtractor';
 
-const linguee = (function () {
-  return {
-    translate(term: string, fromLang: LangCode, toLang: LangCode) {
-      try {
-        const url = Endpoint.createSearchUrl(term, fromLang, toLang);
+const linguee = {
+  translate(term: string, fromLang: LangCode, toLang: LangCode) {
+    try {
+      const url = Endpoint.createSearchUrl(term, fromLang, toLang);
 
-        return new Promise((resolve, reject) => {
-          axios
-            .get(url, { responseType: 'arraybuffer' })
-            .then((response) => {
-              const responseCharset = response.headers['content-type'].match(
-                /.+charset="(?<charset>.*)"$/
-              )?.groups?.charset;
+      return new Promise((resolve, reject) => {
+        axios
+          .get(url, { responseType: 'arraybuffer' })
+          .then((response) => {
+            const responseCharset = response.headers['content-type'].match(
+              /.+charset="(?<charset>.*)"$/
+            )?.groups?.charset;
 
-              if (!responseCharset) {
-                throw new Error('Unrecognazied service response charset.');
-              }
+            if (!responseCharset) {
+              throw new Error('Unrecognazied service response charset.');
+            }
 
-              const data = iconv.decode(
-                Buffer.from(response.data),
-                iconv.encodingExists(responseCharset)
-                  ? responseCharset
-                  : 'utf-8'
+            const data = iconv.decode(
+              Buffer.from(response.data),
+              iconv.encodingExists(responseCharset) ? responseCharset : 'utf-8'
+            );
+
+            try {
+              const extractor =
+                ExtractorsFactory.create<LingueeExtractor>('linguee');
+              const $ = Cheerio.load(
+                `<div id="extractor-wrapper">${data}</div>`
               );
 
-              try {
-                const extractor =
-                  ExtractorsFactory.create<LingueeExtractor>('linguee');
-                const $ = Cheerio.load(
-                  `<div id="extractor-wrapper">${data}</div>`
-                );
+              const result = extractor.run($('#extractor-wrapper'));
 
-                const result = extractor.run($('#extractor-wrapper'));
-
-                return resolve(result);
-              } catch (error) {
-                reject(error);
-              }
-            })
-            .catch((error) => {
+              return resolve(result);
+            } catch (error) {
               reject(error);
-            });
-        });
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    },
-    langs: Lang,
-  };
-})();
+            }
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+};
 
-module.exports = linguee;
+export default linguee;
